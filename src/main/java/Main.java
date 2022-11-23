f4a148d760fab9e8fbeb3f8b9cb8a247cc50cf73
Gobi <biziewskamalgorzata@gmail.com>
2022-11-23 18:50:56 +0100

    json to map

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,31 +9,38 @@ import java.util.*;
 
 public class Main {
 
-    private static final String CONFIG_FILE_NAME = "config.txt";
     private static final String WORLD_LIST_FILE_NAME = "worldcities.csv";
+    private static final String JSON_FILE_NAME = "weather.json";
+
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+        String CONFIG_FILE_NAME = "config.txt";
+        String key = "9a3194d55e70fc631f78dd9ac4187ee5";
+        String city = "Buenos Aires";
+        String lat = "";
+        String lon = "";
 
         List<String> list = readDataFromFileToList(WORLD_LIST_FILE_NAME);
 
         writeDataFromListToFile(CONFIG_FILE_NAME, list);
 
-        Map<String, List<String>> map =  readDataFromFileToMap(CONFIG_FILE_NAME);
+        Map<String, List<String>> map = readDataFromFileToMap(CONFIG_FILE_NAME);
 
-//        Set<Map.Entry<String, List<String>>> entries = map.entrySet();
-//        for (Map.Entry<String, List<String>> entry : entries) {
-//            System.out.println(entry.getKey() + " : " + entry.getValue());
-//        }
-
-
-
+        Set<Map.Entry<String, List<String>>> entries = map.entrySet();
+        for (Map.Entry<String, List<String>> entry : entries) {
+            if (entry.getKey().equals(city)) {
+                lat = entry.getValue().get(0);
+                lon = entry.getValue().get(1);
+            }
+        }
 
 
         try {
-
-            URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=9a3194d55e70fc631f78dd9ac4187ee5");
-            //URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=9a3194d55e70fc631f78dd9ac4187ee5");
+            URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?lat="
+                    + lat + "&lon=" + lon + "&appid=" + key);
+            System.out.println("Url: " + url);
 
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
@@ -55,22 +62,35 @@ public class Main {
                 }
                 scanner.close();
 
-                System.out.println(informationString);
+                String result = String.valueOf(informationString);
 
                 JSONParser parser = new JSONParser();
-                Object obj = parser.parse(String.valueOf(informationString));
-                JSONArray array = new JSONArray();
-                array.add(obj);
+                JSONObject jsonObject = (JSONObject) parser.parse(result);
+//                writeJsonObjectToFile(JSON_FILE_NAME, jsonObject);
+//                System.out.println(jsonObject.get("list"));
 
-                System.out.println(array.get(0));
 
-                JSONObject jsonObject = (JSONObject) array.get(0);
-                System.out.println();
-                System.out.println(jsonObject.get("main"));
+                System.out.println(jsonObject);
+                writeJsonObjectToFile(JSON_FILE_NAME, jsonObject);
+                System.out.println(jsonObject.get("list"));
 
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void writeJsonObjectToFile(String fileName, JSONObject jsonObject) {
+        try (FileWriter fileWriter = new FileWriter(fileName);
+             BufferedWriter writer = new BufferedWriter(fileWriter)) {
+
+            writer.write(String.valueOf(jsonObject));
+            writer.newLine();
+
+            System.out.println("Zapisano dane w pliku: " + fileName);
+
+        } catch (IOException e) {
+            System.err.println("Błąd odczytu/zapisu danych z/do pliku: " + fileName);
         }
     }
 
@@ -101,8 +121,7 @@ public class Main {
 
     private static void writeDataFromListToFile(String fileName, List<String> list) {
         try (FileWriter fileWriter = new FileWriter(fileName);
-             BufferedWriter writer = new BufferedWriter(fileWriter))
-        {
+             BufferedWriter writer = new BufferedWriter(fileWriter)) {
 
             for (int i = 1; i < list.size(); i++) {
                 writer.write(list.get(i));
@@ -140,6 +159,51 @@ public class Main {
         return list;
 
     }
+
+
+//    try {
+//
+//        URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?lat="
+//                + lat + "&lon=" + lon + "&appid=" + key);
+//        System.out.println("Url: " + url);
+//        //URL url = new URL("https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=9a3194d55e70fc631f78dd9ac4187ee5");
+//
+//        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+//        conn.setRequestMethod("GET");
+//        conn.connect();
+//
+//        //Check if connect is made
+//        int responseCode = conn.getResponseCode();
+//
+//        // 200 OK
+//        if (responseCode != 200) {
+//            throw new RuntimeException("HttpResponseCode: " + responseCode);
+//        } else {
+//
+//            StringBuilder informationString = new StringBuilder();
+//            scanner = new Scanner(url.openStream());
+//
+//            while (scanner.hasNext()) {
+//                informationString.append(scanner.nextLine());
+//            }
+//            scanner.close();
+//
+//            System.out.println(informationString);
+//
+//            JSONParser parser = new JSONParser();
+//            Object obj = parser.parse(String.valueOf(informationString));
+//            JSONArray array = new JSONArray();
+//            array.add(obj);
+//
+//            System.out.println(array.get(0));
+//
+//            JSONObject jsonObject = (JSONObject) array.get(0);
+//            System.out.println();
+//            System.out.println(jsonObject.get("list"));
+//        }
+//    } catch (Exception e) {
+//        e.printStackTrace();
+//    }
 
 }
 
